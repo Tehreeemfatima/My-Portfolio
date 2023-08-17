bf88bf802a058f03f143558f8574a690739f9fe6
Daniyalamir <udanial555@gmail.com>
2023-08-17 05:41:41 -0700

    upload in github

---
+++ src/app/Components/About.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div id="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white text-lg text-justify py-[550px] md:py-0'> 
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full '>
+             <div className='pb-8 '>
+                <p className='text-4xl font-bold inline border-b-4  border-gray-500'>About</p>
+             </div>
+
+             <p >As a front-end developer, I specialize in creating visually stunning and user-friendly websites using cutting-edge tools and technologies. My core competencies include proficiency in Chakra UI, Tailwind CSS, Next.js, React, as well as a strong command over JavaScript and TypeScript. With an eye for detail and a passion for excellence,
+                 I strive to deliver outstanding results that meet and exceed client expectations.</p>
+              <br />
+              
+              <p >Throughout my career, I have been involved in a variety of projects ranging from small-scale website development to complex web applications. My expertise in front-end technologies, 
+                combined with my ability to think creatively and approach problems from different angles, allows me to develop unique solutions that cater to the specific needs of each client. With my skills and knowledge, I am confident in my ability to create stunning, high-performance websites that provide an unparalleled user experience.</p>  
+        </div>
+
+
+    </div>
+  )
+}
